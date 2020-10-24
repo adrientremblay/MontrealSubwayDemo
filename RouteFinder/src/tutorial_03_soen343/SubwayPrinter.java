d26c8e6ef5322ca231350884f1edaa1fae1168f2
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-24 14:55:16 -0400

    output file

--- RouteFinder/src/tutorial_03_soen343/SubwayPrinter.java
+++ RouteFinder/src/tutorial_03_soen343/SubwayPrinter.java
@@ -4,11 +4,17 @@ import java.io.*;
 import java.util.*;
 
 public class SubwayPrinter {
-    private PrintStream out;
+    private PrintStream console;
+    private PrintStream file;
     private final int PRICE_PER_STATION = 1; // $cad
     
-    public SubwayPrinter(OutputStream out) {
-        this.out = new PrintStream(out);
+    public SubwayPrinter(OutputStream console) throws FileNotFoundException {
+        this.console = new PrintStream(console);
+        this.file = new PrintStream(new File("output.txt"));
+    }
+    private void println(String output) {
+        this.console.println(output);
+        this.file.println(output);
     }
     
     public void printDirections(List route) {
@@ -16,25 +22,25 @@ public class SubwayPrinter {
         String currentLine = connection.getLineName();
         String previousLine = currentLine;
         
-        out.println("Start out at " + connection.getStation1().getName() + ".");
-        out.println("Get on the " + currentLine + " heading towards " + connection.getStation2().getName() + ".");
+        println("Start out at " + connection.getStation1().getName() + ".");
+        println("Get on the " + currentLine + " heading towards " + connection.getStation2().getName() + ".");
         
         for (int i = 1; i < route.size(); i++) {
             connection = (Connection) route.get(i);
             currentLine = connection.getLineName();
             if (currentLine.equals(previousLine)) {
-                out.println("  Continue past  " + connection.getStation1().getName() + "...");
+                println("  Continue past  " + connection.getStation1().getName() + "...");
             }
             else {
-                out.println("When you get to " + connection.getStation1().getName() + ", get off the " + previousLine + ".");
-                out.println("Switch over to the " + currentLine + ", heading towards " + connection.getStation2().getName() + ".");
+                println("When you get to " + connection.getStation1().getName() + ", get off the " + previousLine + ".");
+                println("Switch over to the " + currentLine + ", heading towards " + connection.getStation2().getName() + ".");
                 previousLine = currentLine;
             }
         }
-        out.println("Get off at " + connection.getStation2().getName() + " and enjoy yourself!");
+        println("Get off at " + connection.getStation2().getName() + " and enjoy yourself!");
 
         // printing price
         int price = PRICE_PER_STATION * route.size() + 1;
-        out.println("Total cost will be " + price + "$");
+        println("Total cost will be " + price + "$");
     }
 }
