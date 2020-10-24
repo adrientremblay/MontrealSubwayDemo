a6c3e9881cdacc7c01a36c5738beb4664eebacbf
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-24 16:48:12 -0400

    Station javadoc

--- RouteFinder/src/tutorial_03_soen343/Station.java
+++ RouteFinder/src/tutorial_03_soen343/Station.java
@@ -1,28 +1,46 @@
 package tutorial_03_soen343;
 
-public class Station
-{
+/**
+ * Represents a station of the train network.
+ * Stations can be compared to one another and
+ * are considered equal if they have the same name.
+ * Stations can be hashed according to their names.
+ */
+public class Station {
     private String name;
-    
+
+    /**
+     * Creates a Station given a name value.
+     * @param name the name of the station
+     */
     public Station(String name) {
         this.name = name;
     }
-    
+
+    /**
+     * Gets the name of the station.
+     * @return the name of the station
+     */
     public String getName() {
         return name;
     }
-    
+
+    /**
+     * Determines if the given station is equal to this station.
+     * Stations are considered equal if they have the same name.
+     * @param obj the Station to compare with this Station
+     * @return true if Stations are equal, false otherwise
+     */
     public boolean equals(Object obj) {
         if (obj instanceof Station) {
             Station otherStation = (Station) obj;
-            if (otherStation.getName().equalsIgnoreCase(name))
-            {
+            if (otherStation.getName().equalsIgnoreCase(name)) {
                 return true;
             }
         }
         return false;
     }
-    
+
     public int hashCode() {
         return name.toLowerCase().hashCode();
     }
