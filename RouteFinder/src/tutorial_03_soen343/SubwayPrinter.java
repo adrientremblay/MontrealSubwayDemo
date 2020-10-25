bb54399b5680671ec70cc80ebc6782fd94fd7e11
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-24 20:15:03 -0400

    SubwayPrinter javadoc

--- RouteFinder/src/tutorial_03_soen343/SubwayPrinter.java
+++ RouteFinder/src/tutorial_03_soen343/SubwayPrinter.java
@@ -3,20 +3,48 @@ package tutorial_03_soen343;
 import java.io.*;
 import java.util.*;
 
+/**
+ * Provides the functionality to convert
+ * a list of connections representing the path taken by
+ * the train from start to destination into a sequence
+ * of steps and facts in plain english.
+ * The cost of the journey is also calculated and
+ * displayed.
+ * These lines are printed to the given OutputStream and additionally
+ * saved in an output file.
+ */
 public class SubwayPrinter {
-    private PrintStream console;
+    private PrintStream out;
     private PrintStream file;
     private final int PRICE_PER_STATION = 1; // $cad
-    
-    public SubwayPrinter(OutputStream console) throws FileNotFoundException {
-        this.console = new PrintStream(console);
+
+    /**
+     * Creates a SubwayPrinter using a given OutputStream.
+     * @param out the OutputStream to print each line to
+     * @throws FileNotFoundException
+     */
+    public SubwayPrinter(OutputStream out) throws FileNotFoundException {
+        this.out = new PrintStream(out);
         this.file = new PrintStream(new File("output.txt"));
     }
+
+    /**
+     * Prints the given String to both the out OutputStream and
+     * to the output file.
+     * @param output the line String to print
+     */
     private void println(String output) {
-        this.console.println(output);
+        this.out.println(output);
         this.file.println(output);
     }
-    
+
+    /**
+     * Prints to the output file and given Output Stream
+     * a series of actions and facts derived from
+     * the List of connections given that represents the
+     * travel from start to destination station.
+     * @param route the list of connections taken
+     */
     public void printDirections(List route) {
         Connection connection = (Connection) route.get(0);
         String currentLine = connection.getLineName();
@@ -39,7 +67,6 @@ public class SubwayPrinter {
         }
         println("Get off at " + connection.getStation2().getName() + " and enjoy yourself!");
 
-        // printing price
         int price = PRICE_PER_STATION * route.size() + 1;
         println("Total cost will be " + price + "$");
     }
